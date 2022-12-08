b064b0e96f64713229069e87a81021566a9aefb1
Angie Rodriguez <angrodri902@gmail.com>
2022-12-08 06:44:57 -0800

    Remove default styles and add nav links

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function About() {
+  return (
+    <div>
+      <h1>My Name is Angie</h1>
+      <p>
+        I\'ve been a front end developer for over 1 year, professionally. I
+        enjoy web development and am excited to keep learning more about
+        software engineering.
+      </p>
+    </div>
+  );
+}
