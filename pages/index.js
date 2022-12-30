a58017212131d9a3ddb37ddfe17b7c9ddd64f7ff
Angie Rodriguez <angrodri902@gmail.com>
2022-12-29 18:06:30 -0800

    Install Sass and adjust styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -15,8 +15,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>
-        <nav>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
           <Link href="/">Home</Link>
           <Link href="/about.js">About Me</Link>
           <Link href="/portfolio.js">Portfolio</Link>
@@ -24,19 +24,57 @@ export default function Home() {
         </nav>
       </header>
 
-      <main>
-        <h1>Angie Rodriguez</h1>
-        <p>Front End Software Engineer based around Portland, OR.</p>
+      <main className={styles.main}>
+        <div className={styles.about}>
+          <h2>Basic Facts</h2>
+          <p>2018</p>
+        </div>
+        <div className={styles.skills}>
+          <h2>Angie Rodriguez</h2>
+          <p>Front End Software Engineer based around Portland, OR.</p>
+        </div>
+        <div>
+          <h1>Angie Rodriguez</h1>
+          <p>Front End Software Engineer based around Portland, OR.</p>
+        </div>
       </main>
 
-      <footer>
-        <nav>
+      <footer className={styles.footer}>
+        <nav className={styles.nav}>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             Powered by{" "}
+            <span>
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                width={72}
+                height={16}
+              />
+            </span>
+          </a>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                width={72}
+                height={16}
+              />
+            </span>
+          </a>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span>
               <Image
                 src="/vercel.svg"
