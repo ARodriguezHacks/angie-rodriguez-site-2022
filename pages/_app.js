04367a95d9a4e01fdf3b42e04d3e32493f8156cc
Angie Rodriguez <angrodri902@gmail.com>
2023-03-12 22:18:56 -0700

    Create Footer component and add React fragments

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,10 @@ import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <>
       <Navigation />
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
 
