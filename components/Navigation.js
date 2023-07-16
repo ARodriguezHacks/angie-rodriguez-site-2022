caea1c56d8cf35c969e56e68f8aa4e236ecdaf83
Angie Rodriguez <angrodri902@gmail.com>
2023-07-15 21:13:13 -0700

    adjust styles for nav on mobile and desktop

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,19 +1,30 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Home.module.scss";
+import { useState } from "react";
 
 export default function Navigation() {
+  const [mobileNav, setMobileNav] = useState(false);
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <div className={styles.navList}>
+        <div className={styles.navListLogo}>
           <ul>
-            <li className={styles.desktopHide}>Menu Mobile</li>
-            <li>
+            <li className={`${styles.tabletHide} ${styles.desktopHide}`}>
+              <Image
+                src="/icons/menu.png"
+                alt="hamburger menu icon"
+                width="64"
+                height="64"
+              />
+            </li>
+            <li className={styles.mobileHide}>
               <Link href="/">Home</Link>
             </li>
           </ul>
         </div>
-        <div className={styles.navList}>
+        <div className={`${styles.navList} ${styles.mobileNavList}`}>
           <ul>
             <li>
               <Link href="/about">About Me</Link>
