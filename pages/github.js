c9a4c677a278671651821ea2525bb754ae02a9f2
Angie Rodriguez <angrodri902@gmail.com>
2023-08-08 09:56:45 -0700

    Adjust github page with API call

--- pages/github.js
+++ pages/github.js
@@ -1,3 +1,41 @@
+import { useState, useEffect } from 'react';
+
 export default function Github() {
-  return <h1>My latest Github works</h1>;
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+  console.log(data)
+
+  useEffect(() => {
+    setLoading(true)
+    async function getData() {
+      const data = await fetch(
+        `${process.env.NEXT_PUBLIC_GITHUB_URL}`, {
+        headers: {
+          "accept": "application/vnd.github+json",
+        },
+      }
+      );
+      const res = await data.json();
+      setData(res);
+    }
+    setLoading(false)
+    getData()
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No profile data</p>
+
+  return (
+    <div>
+      <h1>My latest Github works:</h1>
+      <div>
+        {data.map(dataItem => (
+          <div key={dataItem.id}>
+            <h3>{dataItem.name}</h3>
+            <p>{dataItem.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
