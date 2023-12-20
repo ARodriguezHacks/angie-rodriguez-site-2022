85ba7c17cd182c1f497edf346331d94b80a04068
Angie Rodriguez <angrodri902@gmail.com>
2023-12-19 22:31:04 -0800

    update landing page sections

--- sections/github.js
+++ sections/github.js
@@ -1,21 +1,59 @@
+import { useState, useEffect } from 'react';
+import Image from "next/image";
+// import backgroundPic from "../public/images/background.jpg";
+import profilePic from "../public/images/AngieR_profile_min_2.png";
 import styles from "../styles/Home.module.scss";
 
 export default function Github() {
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+  // console.log(data)
+
+  useEffect(() => {
+    setLoading(true)
+    async function getData() {
+      const data = await fetch(
+        `${process.env.NEXT_PUBLIC_GITHUB_URL}`, {
+        headers: {
+          "accept": "application/vnd.github+json",
+        },
+      }
+      );
+      const res = await data.json();
+      setData(res);
+    }
+    setLoading(false)
+    getData()
+  }, [])
+
+  if (isLoading && !data) return <p>Loading...</p>
+  if (!isLoading && !data) return <p>No profile data</p>
+
   return (
-    <div className={`${styles.sectionFlex} ${styles.github} ${styles.pageMargin}`} id="github">
+    <div className={ `${styles.sectionFlex} ${styles.github} ${styles.pageMargin}` } id="github">
       <div>
-        <picture></picture>
-      </div>
-      <div>
-        <h2 className={styles.headingOne}>
+        <h2 className={ styles.headingOne }>
           Check out my latest Github Works
         </h2>
-        <ul>
-          <li>2021 - 2023: Aisle Rocket</li>
-          <li>2020 - 2021: Gold Leaf Settlement Solutions (Contract)</li>
-          <li>2020 - 2021: WORKPLACE21 (Contract)</li>
-          <li>2020 - 2021: Code for PDX</li>
-        </ul>
+        <div>
+          { data.map(dataItem => (
+            <div key={ dataItem.id }>
+              <div className={ styles.heroProfileImage }>
+                <div>
+                  <Image
+                    src={ profilePic }
+                    alt="Angie Rodriguez profile pic"
+                    width={ 250 }
+                    height={ 250 }
+                    style={ { boxShadow: '10px 10px 10px gray' } }
+                  />
+                </div>
+               </div>
+              <h3>{ dataItem.name }</h3>
+              <p>{ dataItem.description }</p>
+            </div>
+          )) }
+        </div>
       </div>
     </div>
   );
--- sections/hero.js
+++ sections/hero.js
@@ -6,28 +6,26 @@ import profilePic from "../public/images/AngieR_profile_min_2.png";
 
 export default function Hero() {
   return (
-    <div className={`${styles.sectionFlex} ${styles.about} ${styles.pageMargin}`} id="hero">
-      <div className={styles.aboutFlexContainer}>
-        <div className={styles.heroCopy}>
+    <div className={ `${styles.sectionFlex} ${styles.about} ${styles.pageMargin}` } id="hero">
+      <div className={ styles.aboutFlexContainer }>
+        <div className={ styles.heroCopy }>
           <p>Welcome!</p>
-          <h1 className={styles.headingOne}>
+          <h1 className={ styles.headingOne }>
             <span>My name is</span> Angie Rodriguez
           </h1>
-          <p className={styles.heroParagraph}>
+          <p className={ styles.heroParagraph }>
             I&apos;m a software engineer based in the PNW. I love working in
             React.js, TypeScript and more.
           </p>
         </div>
-        <div className={styles.heroProfileImage}>
-          <div>
-            <Image
-              src={profilePic}
-              alt="Angie Rodriguez profile pic"
-              width={500}
-              height={500}
-              style={{ boxShadow: '10px 10px 10px gray' }}
-            />
-          </div>
+        <div className={ styles.heroProfileImage }>
+          <Image
+            src={ profilePic }
+            alt="Angie Rodriguez profile pic"
+            width={ 500 }
+            height={ 500 }
+            style={ { boxShadow: '10px 10px 10px gray' } }
+          />
         </div>
       </div>
     </div>
